f144313ce131edc87931902026852575a3a749e6
chandrulogan <78639242+chandrulogan@users.noreply.github.com>
2022-12-09 09:26:45 +0530

    React to add to cart project

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Footer from './Footer';
+import Header from './Header';
+import Navigation from './Navigation';
+import Section from './Section';
+
+function App() {
+  return (
+    <div className="App">
+      <Section/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
